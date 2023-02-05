a0198291a0e0fc35d041924276858f9d8b88e4d7
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-02-05 23:45:08 +0300

    feat: 1.10 start FSD-structure for the project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,14 @@ import React, {Suspense} from 'react';
 import './styles/index.scss'
 import {Routes, Route, Link} from "react-router-dom";
 
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 
 const App = () => {
-
     const {theme, toggleTheme} = useTheme()
 
     return (
@@ -19,8 +19,8 @@ const App = () => {
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<></>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
 
