c12a47d0716d7aec7648ab77f7d4bbb309bf9018
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-02-13 23:00:23 +0300

    feat: 2.6 Webpack hot module

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -14,6 +14,7 @@ export function buildPlugins(path: string, isDev: boolean): webpack.WebpackPlugi
       }),
       new webpack.DefinePlugin({
         __IS_DEV__: JSON.stringify(isDev)
-      })
+      }),
+      new webpack.HotModuleReplacementPlugin()
   ];
 }
