e5162403527fc66e445957c6869cc128215d4c13
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-02-25 23:49:38 +0300

    feat: 4.1 add Redux-toolkit, Entity and redux tests

---
+++ src/app/providers/StoreProvider/index.ts
@@ -0,0 +1,5 @@
+import { StoreProvider } from './ui/StoreProvider';
+import { createReduxStore } from './config/store';
+import type { StateShema } from './config/StateShema';
+
+export { StoreProvider, createReduxStore, StateShema };
