9788980f5eab180ee52fde260cab4f91cb1c93c3
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2025-03-04 20:36:19 +0300

    feat (2-9): add stylelint and eslint-plugin-i18next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import i18next from "eslint-plugin-i18next";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -20,6 +21,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  i18next.configs["flat/recommended"],
   {
     rules: {
       "react/react-in-jsx-scope": "off",
